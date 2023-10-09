a330743dad2155df38922a99f282628b43df0b9c
Makorii <marcelaromina.d@hotmail.com>
2023-10-08 21:19:58 -0300

    agrego funcion para añadir un nuevo job, con el metodo POST, en la api

--- js/api.js
+++ js/api.js
@@ -1,3 +1,4 @@
+// funcion que trae los datos
 const getJobs = async() => {
     showView("spinner")
     let response = await fetch("https://65214fb8a4199548356d0a7d.mockapi.io/api/jobs")
@@ -7,3 +8,30 @@ const getJobs = async() => {
     }, 2000);
 }
 getJobs()
+
+// funcion que agrega un nuevo job
+const addNewJob = async (id) => {
+    showView("spinner")
+    let response = await fetch("https://65214fb8a4199548356d0a7d.mockapi.io/api/jobs", {
+        method: "POST",
+        body: JSON.stringify({
+            name: $("#title-new-job").value,
+            description: $("#description-new-job").value,
+            image: "https://static.vecteezy.com/system/resources/previews/005/337/799/non_2x/icon-image-not-found-free-vector.jpg",
+            location: $("#location-new-job").value,
+            seniority: $("#seniority-new-job").value,
+            category: $("#category-new-job").value,
+            id: id,
+        }),
+        headers:{
+            "Content-type": "application/json; charset=UTF-8",
+        } 
+    })
+    let data = await response.json()
+    setTimeout(() => {
+        getJobs(data)
+        
+    }, 2000);
+}
+
+$("#submit-new-job").addEventListener("click", () => addNewJob())
